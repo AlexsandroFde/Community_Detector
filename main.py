20ab8780795bb0b27e6d05a536a4f976370a4cf2
AlexsandroFde <alexsandro.ferreira02@aluno.ifce.edu.br>
2024-03-06 20:47:00 -0300

    added simple main interaction

---
+++ main.py
@@ -0,0 +1,16 @@
+from src.detection.detect_communities import detect_communities
+from src.file_reader import file_reader
+
+def main():
+    file_path = input("Informe o nome do arquivo com os dados: ")
+
+    graph = file_reader(file_path)
+
+    communities = detect_communities(graph)
+
+    print("Comunidades encontradas:")
+    for idx, community in enumerate(communities):
+        print(f"Comunidade {idx+1}: {community}")
+
+if __name__ == "__main__":
+    main()
